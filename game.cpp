837f264f9bd5d820df339722cb7fa3495cddb7c9
Hakkahi <antoinegeorges00@gmail.com>
2017-05-26 15:41:46 +0200

    Ajout de la classe game

---
+++ game.cpp
@@ -0,0 +1,28 @@
+#include "game.hpp"
+
+
+Game::Game(Player& p1, Player& p2, int nbLap){
+    player1 = p1;
+    player2 = p2;
+    currentRound = 0;
+    nbRound = nbLap;
+    score.first = 0;
+    score.second = 0;
+}
+
+
+void Game::winRound(int idPlayer){
+    if(idPlayer == 1){
+        score.first++;
+    }
+    else{
+        score.second++;
+    }
+    
+    if(currentRound + 1 < nbRound){
+        currentRound++;
+    }
+    else{
+        std::cout << "C'était le dernier tour FDP" << std::endl;
+    }
+}
